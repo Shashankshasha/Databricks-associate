ab155eab95aa417d561c731cf76691b3a2103136
singhshashank3333@gmail.com <singhshashank3333@gmail.com>
2023-05-29 21:28:01 +0000

    first-sample-table

---
+++ a.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC Create table first_table(
+# MAGIC   Id Int,
+# MAGIC   Name string,
+# MAGIC   Department string,
+# MAGIC   Salary Double)
+# MAGIC   ;
+# MAGIC   
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC show tables first_table;
+
+# COMMAND ----------
+
+
